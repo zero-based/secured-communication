4c9a077611225543f1e5c057dcc4af125d57370b
MonicaTanios <monicatanios@gmail.com>
2019-12-12 22:47:56 +0200

    [AES] Implement AddRndKey PROC

--- AES/addRoundKey.asm
+++ AES/addRoundKey.asm
@@ -1,20 +1,33 @@
 INCLUDE procs.inc			; get procedure prototypes
 INCLUDE vars.inc			; get variables
 
+.data
+arraySize DWORD 16
+
 .code
 
 ; ----------------------------------------------------------
 AddRndKey	PROC,
 			msg		:PTR BYTE,			; Offset of message matrix
-			key		:ptr byte			; Offset of key matrix
+			key		:PTR BYTE			; Offset of key matrix
 ;
 ; It's a simple XOR between the plain text and the round key.
 ; It's the same step in encryption and decryption.
 ; Returns: nothing
 ; ----------------------------------------------------------
-			pushad			; save all registers
+			pushad						; save all registers
 
-			popad			; restore all registers
+			mov		ecx, arraySize		; loop counter 
+			mov		edi, msg
+			mov		esi, key
+XorLoop:	mov		al, BYTE PTR [edi]
+			xor		al, [esi]
+			mov		[edi], al
+			inc		esi
+			inc		edi
+			loop	XorLoop
+
+			popad						; restore all registers
 			ret
 AddRndKey	ENDP
 
